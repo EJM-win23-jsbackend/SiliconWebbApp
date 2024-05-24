245fe83f48ae1b1fec12f9c68f07c9846c67924f
Evelina <krugerevelina@gmail.com>
2024-05-24 13:42:35 +0200

    courses

---
+++ BlazorWebbApp/BlazorWebbApp/Models/DynamicGraphQlResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BlazorWebbApp.Models;
+
+public class DynamicGraphQlResponse
+{
+    [JsonPropertyName("data")]
+    public JsonElement Data { get; set; }
+}
