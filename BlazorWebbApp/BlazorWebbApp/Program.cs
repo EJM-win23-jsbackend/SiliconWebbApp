2cc8ad54f6894a950d1c2b8d852a749aca87552c
Marcus <Marcuswix@yahoo.se>
2024-06-01 08:25:38 +0200

    "Bugfixes"

--- BlazorWebbApp/BlazorWebbApp/Program.cs
+++ BlazorWebbApp/BlazorWebbApp/Program.cs
@@ -47,6 +47,11 @@ builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.Requ
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
+builder.Services.AddServerSideBlazor().AddCircuitOptions(options =>
+{
+    options.DetailedErrors = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
